0bc26c8e0c4e8cbe6532231797a48a155edf793e
alphonsezhang <lovestudy@qq.com>
2024-08-01 09:49:08 +0800

    app/user tmp

--- app/user/biz/service/login.go
+++ app/user/biz/service/login.go
@@ -2,6 +2,10 @@ package service
 
 import (
 	"context"
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"user/biz/dal/mysql"
+	"user/biz/model"
 	user "user/kitex_gen/user"
 )
 
@@ -15,6 +19,20 @@ func NewLoginService(ctx context.Context) *LoginService {
 // Run create note info
 func (s *LoginService) Run(req *user.LoginReq) (resp *user.LoginResp, err error) {
 	// Finish your business logic.
+	if req.Email == "" || req.Password == "" {
+		return nil, errors.New("email or password is empty")
+	}
+	row, err := model.GetByEmail(s.ctx, mysql.DB, req.Email)
+	if err != nil {
+		return nil, err
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(row.PasswordHashed), []byte(req.Password))
+	if err != nil {
+		return nil, err
+	}
+	resp = &user.LoginResp{
+		UserId: int32(row.ID),
+	}
 
-	return
+	return resp, nil
 }
