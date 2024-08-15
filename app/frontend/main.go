0fdfdf467b939defb9963edd5b9848442c446fde
alphonsezhang <lovestudy@qq.com>
2024-08-15 11:22:15 +0800

    添加本地存储日志

--- app/frontend/main.go
+++ app/frontend/main.go
@@ -7,11 +7,13 @@ import (
 	"frontend/infra/mtl"
 	"frontend/infra/rpc"
 	"frontend/middleware"
+	"io"
 	"os"
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/middlewares/server/recovery"
 	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
 	"github.com/cloudwego/hertz/pkg/common/utils"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
 	"github.com/hertz-contrib/cors"
@@ -30,9 +32,18 @@ import (
 func main() {
 	_ = godotenv.Load()
 	// 可观测性初始化
-	mtl.InitMtl()
+	// mtl.InitMtl()
 	rpc.InitClient()
 	address := conf.GetConf().Hertz.Address
+	// 存储日志
+	f, err := os.OpenFile(conf.GetConf().Hertz.LogFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	fileWriter := io.MultiWriter(f, os.Stdout)
+	hlog.SetOutput(fileWriter)
+
 	// 启动追踪，但不启动度量
 	_ = hertzotelprovider.NewOpenTelemetryProvider(
 		hertzotelprovider.WithSdkTracerProvider(mtl.TracerProvider),
@@ -74,6 +85,7 @@ func main() {
 	h.Spin()
 }
 
+// 注册一些通用组件
 func registerMiddleware(h *server.Hertz) {
 	// pprof
 	if conf.GetConf().Hertz.EnablePprof {
