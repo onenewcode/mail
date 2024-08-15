6e40b8a61a04135d4711cfa960216d4edb6d7a6a
alphonsezhang <lovestudy@qq.com>
2024-08-15 18:10:17 +0800

    over wait test

--- app/cart/infra/rpc/client.go
+++ app/cart/infra/rpc/client.go
@@ -1,40 +1,38 @@
 package rpc
 
 import (
-	"sync"
-
 	"cart/conf"
 	cartutils "cart/utils"
+	"common/clientsuite"
 	"rpc_gen/kitex_gen/product/productcatalogservice"
+	"sync"
 
 	"github.com/cloudwego/kitex/client"
-	"github.com/cloudwego/kitex/pkg/rpcinfo"
-	"github.com/cloudwego/kitex/pkg/transmeta"
-	"github.com/cloudwego/kitex/transport"
-	consul "github.com/kitex-contrib/registry-consul"
 )
 
 var (
 	ProductClient productcatalogservice.Client
 	once          sync.Once
+	err           error
+	registryAddr  string
+	serviceName   string
 )
 
 func InitClient() {
 	once.Do(func() {
+		registryAddr = conf.GetConf().Registry.RegistryAddress[0]
+		serviceName = conf.GetConf().Kitex.Service
 		initProductClient()
 	})
 }
 
 func initProductClient() {
-	var opts []client.Option
-	r, err := consul.NewConsulResolver(conf.GetConf().Registry.RegistryAddress[0])
-	cartutils.MustHandleError(err)
-	opts = append(opts, client.WithResolver(r))
-	opts = append(opts,
-		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: conf.GetConf().Kitex.Service}),
-		client.WithTransportProtocol(transport.GRPC),
-		client.WithMetaHandler(transmeta.ClientHTTP2Handler),
-	)
+	opts := []client.Option{
+		client.WithSuite(clientsuite.CommonGrpcClientSuite{
+			RegistryAddr:       registryAddr,
+			CurrentServiceName: serviceName,
+		}),
+	}
 
 	ProductClient, err = productcatalogservice.NewClient("product", opts...)
 	cartutils.MustHandleError(err)
--- app/cart/main.go
+++ app/cart/main.go
@@ -2,45 +2,61 @@ package main
 
 import (
 	"cart/biz/dal"
-	"cart/conf"
-	"cart/infra/rpc"
-	"net"
 	"rpc_gen/kitex_gen/cart/cartservice"
 
+	"cart/conf"
+	"cart/infra/rpc"
+
+	"common/mtl"
+	"common/serversuite"
+	"common/utils"
+	"net"
+	"strings"
+
 	"github.com/cloudwego/kitex/pkg/klog"
-	"github.com/cloudwego/kitex/pkg/rpcinfo"
-	"github.com/cloudwego/kitex/pkg/transmeta"
 	"github.com/cloudwego/kitex/server"
 	"github.com/joho/godotenv"
-	consul "github.com/kitex-contrib/registry-consul"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
+
+var serviceName string
 
 func main() {
 	_ = godotenv.Load()
+	serviceName = conf.GetConf().Kitex.Service
+	mtl.InitLog(&lumberjack.Logger{
+		Filename:   conf.GetConf().Kitex.LogFileName,
+		MaxSize:    conf.GetConf().Kitex.LogMaxSize,
+		MaxBackups: conf.GetConf().Kitex.LogMaxBackups,
+		MaxAge:     conf.GetConf().Kitex.LogMaxAge,
+	})
+	mtl.InitTracing(serviceName)
+	mtl.InitMetric(serviceName, conf.GetConf().Kitex.MetricsPort, conf.GetConf().Registry.RegistryAddress[0])
 	rpc.InitClient()
 	dal.Init()
 	opts := kitexInit()
+
 	svr := cartservice.NewServer(new(CartServiceImpl), opts...)
+
 	err := svr.Run()
 	if err != nil {
 		klog.Error(err.Error())
 	}
 }
+
 func kitexInit() (opts []server.Option) {
 	// address
-	addr, err := net.ResolveTCPAddr("tcp", conf.GetConf().Kitex.Address)
+	address := conf.GetConf().Kitex.Address
+	if strings.HasPrefix(address, ":") {
+		localIp := utils.MustGetLocalIPv4()
+		address = localIp + address
+	}
+	addr, err := net.ResolveTCPAddr("tcp", address)
 	if err != nil {
 		panic(err)
 	}
-	opts = append(opts, server.WithServiceAddr(addr))
 	opts = append(opts,
-		server.WithMetaHandler(transmeta.ServerHTTP2Handler),
-		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: conf.GetConf().Kitex.Service}),
-	)
-	r, err := consul.NewConsulRegister(conf.GetConf().Registry.RegistryAddress[0])
-	if err != nil {
-		klog.Fatal(err)
-	}
-	opts = append(opts, server.WithRegistry(r))
+		server.WithServiceAddr(addr),
+		server.WithSuite(serversuite.CommonServerSuite{CurrentServiceName: serviceName, RegistryAddr: conf.GetConf().Registry.RegistryAddress[0]}))
 	return
 }
